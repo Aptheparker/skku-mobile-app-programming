4836e4da1226ee720dd3c396db27a76f24937780
aptheparker <aptheparker@gmail.com>
2023-07-08 11:19:18 +0900

    comity ignored

--- comity/app/src/main/java/com/example/comity/SelectedStudyGroupsViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.comity
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import com.example.comity.ui.home.StudyGroup
-
-class SelectedStudyGroupsViewModel : ViewModel() {
-    val selectedStudyGroups: MutableLiveData<List<StudyGroup>> = MutableLiveData()
-}
